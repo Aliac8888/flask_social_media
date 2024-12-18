bc89be30dd0af6cde9c657b8bca27a179facfaa9
esdmr <esdmr0@gmail.com>
2024-12-18 11:28:27 +0330

    feat(backend): add user graph visualization

---
+++ backend/visualize.py
@@ -0,0 +1,12 @@
+from json import dumps
+from db import db
+
+print("digraph U {")
+
+for i in db.users.find({}):
+    print(f'{dumps(str(i["_id"]))} [label={dumps(f"{i["_id"]}\n{i["name"]}\n{i["email"]}")}];')
+
+    for j in i["friends"]:
+        print(f'{dumps(str(i["_id"]))} -> {dumps(str(j))};')
+
+print("}")
