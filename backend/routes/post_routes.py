98adfeea2d7516bdc77ca6b4a78003649caaafb6
esdmr <esdmr0@gmail.com>
2024-12-18 11:28:15 +0330

    feat(backend): add user posts and feed

--- backend/routes/post_routes.py
+++ backend/routes/post_routes.py
@@ -1,10 +1,11 @@
 from flask_openapi3.models.tag import Tag
 from flask_openapi3.blueprint import APIBlueprint
+from backend.models.user import UserId, UserNotFound
 from models.post import *
 from db import db
 from bson import ObjectId
 import datetime
-
+# mm
 posts_tag = Tag(name="posts")
 bp = APIBlueprint("post", __name__, url_prefix="/posts")
 
@@ -13,6 +14,27 @@ bp = APIBlueprint("post", __name__, url_prefix="/posts")
 def list_posts():
     posts = db.posts.find({}).to_list()
     return PostsList(posts=posts).model_dump()
+
+@bp.get("/", tags=[posts_tag], responses={200: PostsList})
+def get_posts(query: UserId):
+    posts = db.posts.find({"author": {"$eq": ObjectId(query.user_id)}}).to_list()
+
+    return PostsList(posts=posts).model_dump()
+
+
+@bp.get(
+    "/feed", tags=[posts_tag], responses={200: PostsList, 404: UserNotFound}
+)
+def get_feed(query: UserId):
+    user = db.users.find_one({"_id": ObjectId(query.user_id)})
+
+    if user is None:
+        return UserNotFound().model_dump(), 404
+
+    posts = db.posts.find({"author": {"$in": user["friends"]}}).to_list()
+
+    return PostsList(posts=posts).model_dump()
+
 
 
 @bp.post("/", tags=[posts_tag], responses={201: PostId})
@@ -45,15 +67,23 @@ def get_post(path: PostId):
 
 @bp.patch("/<post_id>", tags=[posts_tag], responses={204: None, 404: PostNotFound})
 def update_post(path: PostId, body: PostPatch):
+    now = datetime.datetime.now(datetime.UTC)
+
     result = db.posts.update_one(
         {"_id": ObjectId(path.post_id)},
-        {"$set": body.model_dump(exclude_none=True)},
+        {
+            "$set": {
+                **body.model_dump(exclude_none=True),
+                "modification_time": now,
+            }
+        },
     )
 
     if result.matched_count < 1:
         return PostNotFound().model_dump(), 404
 
     return "", 204
+
 
 @bp.delete("/<post_id>", tags=[posts_tag], responses={204: None, 404: PostNotFound})
 def delete_post(path: PostId):
