31241735cec79e59cc418b2d9babf0cc4c3184ce
esdmr <esdmr0@gmail.com>
2024-12-22 21:39:46 +0330

    fix: setup hey-api api baseUrl

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,9 @@
 import {render} from 'preact';
 import {App} from './App.js';
+import {client} from './api/index.js';
+
+client.setConfig({
+	baseUrl: String(import.meta.env.SOCIAL_FE_BE_URL),
+});
 
 render(<App />, document.body);
