c1f0554f50f1cc985e0e8f62d31336c46ef24544
Aliac <alimehdizadeh822@gmail.com>
2024-12-23 23:57:42 +0330

    feat: styles for postview added

--- frontend/src/components/PostView.tsx
+++ frontend/src/components/PostView.tsx
@@ -1,21 +1,26 @@
 import type {Post, User} from '../api/index.js';
 import {UserView} from './UserView.js';
+import styles from './PostView.module.css';
 
 export function PostView({post}: {post: Post}) {
-	return (
-		<div
-			class={`post-view ${post.creation_time === post.modification_time ? '' : 'post-view--show-modification-time'}`}
-		>
-			<div class="post-view__header">
-				<UserView user={post.author} />
-				<div class="post-view__creation-time">
-					{new Date(post.creation_time).toLocaleString()}
-				</div>
-				<div class="post-view__modification-time">
-					{new Date(post.modification_time).toLocaleString()}
-				</div>
-			</div>
-			<div class="post-view__body">{post.content}</div>
-		</div>
-	);
+		return (
+    <div
+      className={`${styles['post-view']} ${
+        post.creation_time === post.modification_time
+          ? ''
+          : styles['post-view--show-modification-time']
+      }`}
+    >
+      <div className={styles['post-view__header']}>
+        <UserView user={post.author} />
+        <div className={styles['post-view__creation-time']}>
+          {new Date(post.creation_time).toLocaleString()}
+        </div>
+        <div className={styles['post-view__modification-time']}>
+          {new Date(post.modification_time).toLocaleString()}
+        </div>
+      </div>
+      <div className={styles['post-view__body']}>{post.content}</div>
+    </div>
+  );
 }
