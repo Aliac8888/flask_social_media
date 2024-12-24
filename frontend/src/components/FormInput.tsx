0cf0bc508e32a4094c09ac1e805218c99da8eac6
esdmr <esdmr0@gmail.com>
2024-12-25 00:35:47 +0330

    fix: weird ts error

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,7 +1,9 @@
 import {useId} from 'preact/hooks';
 import type {JSX} from 'preact/jsx-runtime';
 
-export const FormInput = (p: JSX.IntrinsicElements['input']) => {
+export const FormInput = (
+	p: JSX.IntrinsicElements['input'] & {label: string},
+) => {
 	const id = p.id ?? useId();
 
 	return (
