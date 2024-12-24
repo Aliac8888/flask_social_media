671cd954304f99694c6818c054aafe4b6093aee2
esdmr <esdmr0@gmail.com>
2024-12-24 23:24:25 +0330

    feat: add user provider to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,15 @@
 import {render} from 'preact';
 import {App} from './App.js';
 import {client} from './api/index.js';
+import {UserProvider} from './user.js';
 
 client.setConfig({
 	baseUrl: String(import.meta.env.SOCIAL_FE_BE_URL),
 });
 
-render(<App />, document.body);
+render(
+	<UserProvider>
+		<App />
+	</UserProvider>,
+	document.body,
+);
