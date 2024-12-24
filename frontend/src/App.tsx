873652ce9ca6fdc90392f3ad1e31aed1685c6d66
esdmr <esdmr0@gmail.com>
2024-12-25 01:43:08 +0330

    fix: update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import {PostView} from './components/PostView.js';
 import {postGet, postPost, postPostIdPatch, type Post} from './api/index.js';
 import {useAsyncEffect} from './async-effect.js';
 import styles from './App.module.css';
+import {ProfilePage} from './components/ProfilePage.js';
+import {AuthPage} from './components/AuthPage.js';
+import {useUser} from './user.js';
 
 export function App() {
 	const [posts, setPosts] = useState<Post[]>([]);
@@ -41,31 +44,38 @@ export function App() {
 	}
 
 	return (
-    <div className={styles['app-container']}>
-      <h1 className={styles.header}>Posts</h1>
-      
-      {/* New Post Form */}
-      <div className={styles['create-post']}>
-        <h2>Create New Post</h2>
-        <textarea
-          value={newPostBody}
-          onInput={(event) => setNewPostBody(event.currentTarget.value)}
-          placeholder="Enter post content"
-        />
-        <button onClick={handleCreatePost}>Create Post</button>
-      </div>
+		<div className={styles['app-container']}>
+			<AuthPage />
+			{auth && <ProfilePage userId={auth.user.id} />}
+			<h1 className={styles.header}>Posts</h1>
 
-      {/* List of Posts */}
-      <div className={styles['posts-list']}>
-        {posts.map((post) => (
-          <div key={post.id} className="post-container">
-            <PostView post={post} />
-            <button onClick={() => handleUpdatePost(post.id)}>
-              Star Post
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			{/* New Post Form */}
+			<div className={styles['create-post']}>
+				<h2>Create New Post</h2>
+				<textarea
+					value={newPostBody}
+					onInput={(event) => {
+						setNewPostBody(event.currentTarget.value);
+					}}
+					placeholder="Enter post content"
+				/>
+				<button onClick={handleCreatePost}>Create Post</button>
+			</div>
+
+			{/* List of Posts */}
+			<div className={styles['posts-list']}>
+				{posts.map((post) => (
+					<div key={post.id} className="post-container">
+						<PostView
+							post={post}
+							onUpdate={async () => {
+								const {data} = await postGet();
+								setPosts(data?.posts ?? []);
+							}}
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
