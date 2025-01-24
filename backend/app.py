ca94530bbd0b8f697692a27275ed82700eaab533
esdmr <esdmr0@gmail.com>
2025-01-24 17:19:24 +0330

    chore: remove redundant _routes from file names

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_openapi3.openapi import OpenAPI
 from plugins import *
-from routes import user_routes, post_routes, comment_routes
+from routes import user, post, comment
 from config import be_host, be_port, jwt_secret, jwt_expiry
 from flask_bcrypt import Bcrypt
 
@@ -28,9 +28,9 @@ def redirect_to_openapi():
 
 
 # Register blueprints
-app.register_api(user_routes.bp)
-app.register_api(post_routes.bp)
-app.register_api(comment_routes.bp)
+app.register_api(user.bp)
+app.register_api(post.bp)
+app.register_api(comment.bp)
 
 if __name__ == "__main__":
     app.run(be_host, be_port, debug=True)
